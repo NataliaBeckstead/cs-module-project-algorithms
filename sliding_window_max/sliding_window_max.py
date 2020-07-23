c33685f34d3c120c83d82ef9d96f0ae96e6f65ed
Natalia Beckstead <toosmiravilla@gmail.com>
2020-07-23 16:31:06 -0600

    windows with deque

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,14 +2,29 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+from collections import deque
+
 def sliding_window_max(nums, k):
+    # result = [0] * (len(nums) - k + 1)
+    # for i in range(0, len(nums) - k + 1):
+    #     max_num = nums[i]
+    #     for j in range(i, i + k):
+    #         if max_num < nums[j]:
+    #             max_num = nums[j]
+    #     result[i] = max_num
+    # return result
     result = []
-    for i in range(0, len(nums) - k + 1):
-        max_num = nums[i]
-        for j in range(i, i + k):
-            if max_num < nums[j]:
-                max_num = nums[j]
-        result.append(max_num)
+    q = deque()
+
+    for i in range(len(nums)):
+        while q and nums[i] > nums[q[-1]]:
+            q.pop()
+        q.append(i)
+        if i - q[0] >= k:
+            q.popleft()
+        if i >= k - 1:
+            result.append(nums[q[0]])
+
     return result
 
 
